abe5fcde5d22e1a43d5627f2b60d5b060aa84030
Maciej Szewczyk <maciej.szewczyk@stermedia.pl>
2020-03-05 17:19:13 +0100

    initiall

---
+++ Manager/TranslationInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Lexik\Bundle\TranslationBundle\Manager;
+
+/**
+ * Translation interface.
+ *
+ * @author Cédric Girard <c.girard@lexik.fr>
+ */
+interface TranslationInterface
+{
+    /**
+     * @return string
+     */
+    public function getLocale();
+
+    /**
+     * @return string
+     */
+    public function getContent();
+
+    /**
+     * @return boolean
+     */
+    public function isConfirmed();
+}
